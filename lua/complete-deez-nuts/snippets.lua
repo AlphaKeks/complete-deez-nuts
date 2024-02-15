6068b91a7c629c373408130eae471769bc6ae728
AlphaKeks <alphakeks@dawn.sh>
2024-02-15 07:58:05 +0100

    fix: remove print statement

--- lua/complete-deez-nuts/snippets.lua
+++ lua/complete-deez-nuts/snippets.lua
@@ -8,7 +8,6 @@ local M = {}
 ---
 ---@see :help vim.snippet.expand()
 function M.expand(snippet, start_col, end_col)
-  print("snippet", snippet, "start_col", start_col, "end_col", end_col)
   local current_buf = vim.api.nvim_get_current_buf()
   local current_win = vim.api.nvim_get_current_win()
   local cursor = vim.api.nvim_win_get_cursor(current_win)
